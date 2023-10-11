89fce062fd852b9bce6370d3d5eb8d0c211bedc4
JoshuaMeza <joshuameza2001@gmail.com>
2023-10-11 15:40:45 -0600

    AMS-7 Final adjustments

--- ams_backend/src/main/java/com/gate_software/ams_backend/controller/HelloController.java
+++ ams_backend/src/main/java/com/gate_software/ams_backend/controller/HelloController.java
@@ -22,16 +22,4 @@ public class HelloController {
     public ResponseEntity<String> helloWorld() {
         return new ResponseEntity<>("Hello world!", HttpStatus.OK);
     }
-
-    private void testMails() {
-        String email = "receptor-email@mail";
-
-        Context na_context = new Context();
-        na_context.setVariable("date", "11/09/2023");
-        na_context.setVariable("time", "8:55 a.m.");
-        na_context.setVariable("users", new String[]{
-                "Antonio Linares Flores", "Rosa Martínez Ocampo", "Fatima Dieguez Llanos"
-        });
-        emailService.sendNonAttendanceEmail(email, na_context);
-    }
 }
